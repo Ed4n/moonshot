efc789e8293acb8326596de9b1c4d87117def9c4
Edgardo Valencia <edgardo.avalencia@gmail.com>
2024-12-19 13:39:16 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Edgardo Valencia on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
